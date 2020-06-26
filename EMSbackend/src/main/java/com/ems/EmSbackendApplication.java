001014e5e08663d8cad1bde0bb7964d8800c24d1
Zabith <zabith15514@gmail.com>
2020-06-26 15:04:27 +0530

    first commit

---
+++ EMSbackend/src/main/java/com/ems/EmSbackendApplication.java
@@ -0,0 +1,13 @@
+package com.ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmSbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmSbackendApplication.class, args);
+	}
+
+}
